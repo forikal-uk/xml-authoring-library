75fe9324d9b23d9491f56a105e130e1cac0c539c
John (Arcus) <john@arcuscompliance.com>
2018-07-02 07:24:21 +0100

    Pull up reusable gApi Credentials parsing methods.
    So, they can be reused and encourage consistency.
    
    See: https://github.com/xmlsquad/xml-authoring-library/issues/4#issuecomment-401684178

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -11,13 +11,19 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Yaml;
 
 /**
  * Class AbstractCommand
  */
 abstract class AbstractCommand extends Command
 {
+
+    /**
+     * The name of fallback configuration file for a case when the API files paths are not specified in the input
+     */
     const DEFAULT_CONFIG_FILENAME = 'XmlAuthoringProjectSettings.yaml';
+
 
     /**
      * @string Appended to directories to go up a level.
@@ -304,4 +310,277 @@ abstract class AbstractCommand extends Command
 
         $output->writeln($this->getHelper('formatter')->formatBlock($message, 'error'));
     }
+
+    /**
+     * From ping-drive command
+     *
+     */
+
+    /**
+     * Attempts to find GApiOAuthSecretFile from option or config.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return string|null If the input is incorrect, null is returned. Otherwise an string is returned.
+     */
+    protected function findGApiOAuthSecretFileValue(InputInterface $input, OutputInterface $output): ?string
+    {
+        $needToParseConfigFile = false;
+
+        $gApiOAuthSecretFile = $this->getGApiOAuthSecretFileOption($input);
+        if ($gApiOAuthSecretFile === null) {
+            $needToParseConfigFile = true;
+            if ($output->isVerbose()) {
+                $output->writeln('The client secret file path is not specified, will try to get the path from a configuration file');
+            }
+        }
+
+
+        // If the API file paths are not specified, find and read a configuration file
+        if ($needToParseConfigFile) {
+            if(!$this->isGApiAuthConfigOptionsFindable($output)){
+                return null;
+            }
+
+            if ($gApiOAuthSecretFile === null) {
+
+                if ($this->findGApiOAuthSecretFileConfig($output)) {
+                    $gApiOAuthSecretFile = $this->findGApiOAuthSecretFileConfig($output);
+                } else {
+                    $this->writeError($output, 'The client secret file is specified neither in the CLI arguments nor in'
+                        . ' the configuration file');
+                    return null;
+                }
+            }
+
+        }
+
+        return $gApiOAuthSecretFile;
+    }
+
+
+    /**
+     * Attempts to find GApiAccessTokenFile from option or config.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return stirng|null If the input is incorrect, null is returned. Otherwise an string is returned.
+     */
+    protected function findGApiAccessTokenFileValue(InputInterface $input, OutputInterface $output): ?string
+    {
+
+        $needToParseConfigFile = false;
+
+
+        $gApiAccessTokenFile = $this->getGApiAccessTokenFileOption($input);
+        if ($gApiAccessTokenFile === null) {
+            $needToParseConfigFile = true;
+            if ($output->isVerbose()) {
+                $output->writeln('The access token file path is not specified, will try to get the path from a configuration file');
+            }
+        }
+
+        // If the API file paths are not specified, find and read a configuration file
+        if ($needToParseConfigFile) {
+            if(!$this->isGApiAuthConfigOptionsFindable($output)){
+                return null;
+            }
+
+            if ($gApiAccessTokenFile === null && $this->findGApiAccessTokenFileConfig($output)) {
+                $gApiAccessTokenFile = $this->findGApiAccessTokenFileConfig($output);
+            }
+        }
+
+        return $gApiAccessTokenFile;
+    }
+
+
+
+    /**
+     * Attempts to find config setting for [gApiAccessTokenFile].
+     *
+     * @param OutputInterface $output
+     * @return mixed|null
+     */
+    protected function findGApiAccessTokenFileConfig(OutputInterface $output)
+    {
+        try {
+            $dataFromConfigFile = $this->extractGApiAuthConfigSettings($this->getDataFromConfigFile($output), $output);
+        } catch (\RuntimeException $exception) {
+            $this->writeError($output, 'Couldn\'t read a configuration file: '.$exception->getMessage());
+            return null;
+        }
+
+
+        if (isset($dataFromConfigFile['gApiAccessTokenFile'])) {
+            return $dataFromConfigFile['gApiAccessTokenFile'];
+
+        }
+        //else
+        return null;
+    }
+
+    /**
+     * Attempts to find [gApiOAuthSecretFile] config setting.
+     *
+     *
+     * @param OutputInterface $output
+     * @return mixed|null|string
+     */
+    protected function findGApiOAuthSecretFileConfig(OutputInterface $output)
+    {
+        try {
+            $dataFromConfigFile = $this->extractGApiAuthConfigSettings($this->getDataFromConfigFile($output), $output);
+        } catch (\RuntimeException $exception) {
+            $this->writeError($output, 'Couldn\'t read a configuration file: '.$exception->getMessage());
+            return null;
+        }
+
+        if (isset($dataFromConfigFile['gApiOAuthSecretFile'])) {
+            return $dataFromConfigFile['gApiOAuthSecretFile'];
+
+        }
+        //else
+        return null;
+    }
+
+
+
+
+    protected function isGApiAuthConfigOptionsFindable(OutputInterface $output)
+    {
+        try {
+            if ($this->extractGApiAuthConfigSettings($this->getDataFromConfigFile($output), $output)){
+                return true;
+            }
+        } catch (\RuntimeException $exception) {
+            $this->writeError($output, 'Couldn\'t read a configuration file: '.$exception->getMessage());
+            return false;
+        }
+
+
+    }
+
+    /**
+     * Extract GApiAuth settings from array of config data.
+     *
+     * @param array $configData
+     * @param OutputInterface $output
+     * @return array Empty if no settings extracted.
+     */
+    protected function extractGApiAuthConfigSettings(array $configData, OutputInterface $output)
+    {
+        $options = [];
+
+        // Parsing paths
+        foreach (array('gApiOAuthSecretFile', 'gApiAccessTokenFile') as $option) {
+            if (!isset($configData['google'][$option])) {
+                continue;
+            }
+            if (!is_string($path = $configData['google'][$option])) {
+                $this->writeError($output, 'The google.'.$option.' option value from the configuration file is not a string');
+                continue;
+            }
+
+            $options[$option] = $this->getFullPath(dirname($this->findConfigFile($output)), $path);
+        }
+
+        return $options;
+    }
+
+    /**
+     * Gets options values from a configuration file
+     *
+     * @param OutputInterface $output
+     * @return array[]|null Options values; null means that a configuration file wasn't found. The keys are:
+     *  - gApiOAuthSecretFile (string|null)
+     *  - gApiAccessTokenFile (string|null)
+     * @throws \RuntimeException If a configuration file can't be read or parsed
+     */
+    protected function getDataFromConfigFile(OutputInterface $output): ?array
+    {
+
+        if ($this->findConfigFile($output) === null) {
+            if ($output->isVerbose()) {
+                $output->writeln('A configuration file `'.$this->getCommandStaticConfigFilename().'`'
+                    . ' exists neither in the current directory nor in any parent directory');
+            }
+            return null;
+        }
+
+
+        if ($output->isVerbose()) {
+            $output->writeln('Reading options from the `' . $this->findConfigFile($output) . '` configuration file');
+        }
+
+        try {
+            $configData = Yaml\Yaml::parseFile($this->findConfigFile($output));
+        } catch (Yaml\Exception\ParseException $exception) {
+            throw new \RuntimeException('Couldn\'t parse the configuration file: '.$exception->getMessage());
+        }
+
+        return $configData;
+    }
+
+
+
+    /**
+     * Finds a configuration file within the current working directory and its parents
+     *
+     * @param OutputInterface $output An output to print status
+     * @return string|null The file path. Null means that a file can't be found
+     * @throws \RuntimeException
+     */
+    protected function findConfigFile(OutputInterface $output): ?string
+    {
+        $directory = @getcwd();
+        if ($directory === false) {
+            throw new \RuntimeException('Can\'t get the working directory path. Make sure the working directory is readable.');
+        }
+
+        for ($i = 0; $i < 10000; ++$i) { // `for` protects from an infinite loop
+            $file = $directory . DIRECTORY_SEPARATOR . $this->getCommandStaticConfigFilename();
+
+            if (@is_file($file)) {
+                return $file;
+            }
+
+            $parentDirectory = @realpath($directory.DIRECTORY_SEPARATOR.'..'); // Gets the parent directory path
+
+            // Check whether the parent directory is restricted
+            if ($parentDirectory === false) {
+                if ($output->isVerbose()) {
+                    $output->writeln('The parent directory of `'.$directory.'` is restricted (maybe by open_basedir)'
+                        . ', so the search of a configuration file can\'t be proceeded');
+                }
+                break;
+            }
+
+            if ($parentDirectory === $directory) break; // Check whether the current directory is a root directory
+            $directory = $parentDirectory;
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Converts a relative file path to a full path
+     *
+     * @param string $contextPath A directory path from where the relative path is given
+     * @param string $targetPath The relative path
+     * @return string The full path
+     */
+    protected function getFullPath($contextPath, $targetPath): string
+    {
+        if ($this->filesystem->isAbsolutePath($targetPath)) {
+            return $targetPath;
+        }
+
+        return rtrim($contextPath, '/\\').DIRECTORY_SEPARATOR.$targetPath;
+    }
+
+
+
+
 }
